34892e3601edad789f513770e618bf7fe7bbfea9
Gabi Gergely <gabi@purchasingplatform.com>
2015-05-06 17:39:55 -0500

    Define the ApiError class to store the object it's being passed to it for future retrieval

--- lib/avalara/errors.rb
+++ lib/avalara/errors.rb
@@ -4,5 +4,12 @@ module Avalara
   class Error < StandardError       ; end
   class TimeoutError < Error        ; end
   class NotImplementedError < Error ; end
-  class ApiError < Error            ; end
+
+  class ApiError < Error
+    attr_reader :object
+
+    def initialize(object)
+      @object = object
+    end
+  end
 end
